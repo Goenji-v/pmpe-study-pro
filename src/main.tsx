f4b9c2c298c309eab244f3acbaedf715e6d886ac
Goenji-v <114021868+Goenji-v@users.noreply.github.com>
2026-09-22 16:01:49 -0300

    feat: proteger aplicação contra tela branca

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { AppErrorBoundary } from "./components/AppErrorBoundary";
 
 import "./global.css";
 import "./styles/mobile.css";
@@ -82,6 +83,8 @@ ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
   <React.StrictMode>
-    <App />
+    <AppErrorBoundary>
+      <App />
+    </AppErrorBoundary>
   </React.StrictMode>
 );
